54d7f40342bf0011c84a5454d68bbdd63352ac92
Sudarshan S <senthilsudarshan@gmail.com>
2024-03-13 16:04:23 +0530

    remove API keys

---
+++ components/ChatInput.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
+import { useState } from "react"
+
+type Props = {
+	id: string
+}
+
+function ChatInput({ id }: Props) {
+	const [prompt, setPrompt] = useState("")
+	return (
+		<div>
+			<form className="p-5 space-x-5 flex-1">
+				<input
+					value={prompt}
+					onChange={(e) => setPrompt(e.target.value)}
+					type="text"
+					placeholder="Type your message here..."
+				/>
+
+				<button type="submit">
+					<PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+				</button>
+			</form>
+			<div>{/* Model Selection */}</div>
+		</div>
+	)
+}
+
+export default ChatInput
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,9 +5,20 @@ import NewChatButton from "./NewChatButton"
 import { useSession } from "next-auth/react"
 import Image from "next/image"
 import { signOut } from "next-auth/react"
+import { useCollection } from "react-firebase-hooks/firestore"
+import { collection, orderBy, query } from "firebase/firestore"
+import { db } from "@/firebase"
+import ChatRow from "./ChatRow"
 
 function Sidebar() {
 	const { data: session } = useSession()
+	const [chats, loading, error] = useCollection(
+		session &&
+			query(
+				collection(db, "users", session.user?.email!, "chats"),
+				orderBy("createdAt", "asc")
+			)
+	)
 	return (
 		<div className="p-2 flex flex-col h-screen">
 			<div className="flex-1">
@@ -18,6 +29,9 @@ function Sidebar() {
 					<div>{/* Model Selection */}</div>
 
 					{/* Chat Rows */}
+					{chats?.docs.map((chat) => (
+						<ChatRow key={chat.id} id={chat.id} />
+					))}
 				</div>
 			</div>
 			{session && (
